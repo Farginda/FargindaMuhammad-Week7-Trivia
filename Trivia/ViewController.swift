1f68429a6713a86c882af5d647a9fc0e83723a9f
Farginda <Farginda@luuks-macbook-pro.local>
2018-12-10 13:04:28 +0100

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Farginda on 12/10/18.
+//  Copyright © 2018 Farginda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
